1e8bbca34aee2ea150cd2faa2ab4af707c6c8b9f
maui <maui@devf.mx>
2019-06-11 22:07:30 -0600

    agregar CRUD de Películas

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+    información básica de las películas como, nombre, género, actores y descripción. Tu eres el encargado de diseñar la base de datos para esta app.
+*/
+
 require('dotenv').config();
 require('./database');
 const { PORT, app } = require('./server');
